7af2223bec1ec8281e909e1a43b5999879279d61
Nicholas Huynh <nickhuynhq@gmail.com>
2023-01-08 13:15:29 -0500

    Create pages template

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { CampaignDetails, CreateCampaign, Home, Profile } from './pages'
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
       <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
           Navbar
           <Routes>
-            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/" element={<Home />} />
           </Routes>
       </div>
     </div>
