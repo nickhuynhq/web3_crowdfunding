7f34415dccc04df409b197d984b8395fa6a46c6d
Nicholas Huynh <nickhuynhq@gmail.com>
2023-01-11 12:16:31 -0500

    Fix type in getDonations function

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -87,7 +87,7 @@ export const StateContextProvider = ({ children }) => {
   };
 
   const getDonations = async (pId) => {
-    const donations = await contract.all("getDonators", pId);
+    const donations = await contract.call("getDonators", pId);
     const numberOfDonations = donations[0].length;
 
     let parsedDonations = [];
