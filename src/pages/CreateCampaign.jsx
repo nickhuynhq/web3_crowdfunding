ccc13773cd8939b87e81bcbc7bc6a41afa538421
Nicholas Huynh <nickhuynhq@gmail.com>
2023-03-09 15:30:51 -0500

    Fix styling on CreateCampaign Page

--- src/pages/CreateCampaign.jsx
+++ src/pages/CreateCampaign.jsx
@@ -48,7 +48,7 @@ const CreateCampaign = () => {
   };
 
   return (
-    <div className="bg-[#1c1c24] flex justify-center items-center flex-col h-[80%] rounded-[10px] sm:p-10 p-4">
+    <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
       {isLoading && <Loader />}
 
       {address ? (
