9c0497d73aaf522e8af26aacd20ebbda21145f23
Nicholas Huynh <nickhuynhq@gmail.com>
2023-11-02 15:00:13 -0400

    Fix build issues

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,21 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   define: {
-    global: "globalThis",
+    global: (() => {
+      let globalVariable = "globalThis";
+      try {
+        // Try to import @safe-global/safe-apps-provider
+        require.resolve("@safe-global/safe-apps-provider");
+        // Try to import @safe-global/safe-apps-sdk
+        require.resolve("@safe-global/safe-apps-sdk");
+        // If both modules are found, return the custom global variable
+        globalVariable = "global";
+      } catch (e) {
+        // If either module is not found, fallback to globalThis
+        globalVariable = "globalThis";
+      }
+      return globalVariable;
+    })(),
     "process.env": {},
   },
   build: {
