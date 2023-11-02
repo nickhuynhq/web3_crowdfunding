1004007d340fe9d21184d7ec54d0f41101f42fa8
Nicholas Huynh <nickhuynhq@gmail.com>
2023-11-02 14:39:07 -0400

    Refactor for Thirdweb API key, and use sepolia

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { Sepolia } from "@thirdweb-dev/chains";
 import { StateContextProvider } from "./context";
 import App from "./App";
 import "./index.css";
@@ -9,7 +10,11 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+  <ThirdwebProvider
+    desiredChainId={ChainId.Sepolia}
+    activeChain={Sepolia}
+    clientId={import.meta.env.VITE_CLIENT_ID}
+  >
     <Router>
       <StateContextProvider>
         <App />
