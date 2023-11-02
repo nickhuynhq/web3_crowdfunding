9a16d8a6ff3bf019355fd8d723b7dde945bb8ed0
Nicholas Huynh <nickhuynhq@gmail.com>
2023-11-02 15:19:32 -0400

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -5,27 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   define: {
-    global: (() => {
-      let globalVariable = "globalThis";
-      try {
-        // Try to import @safe-global/safe-apps-provider
-        require.resolve("@safe-global/safe-apps-provider");
-        // Try to import @safe-global/safe-apps-sdk
-        require.resolve("@safe-global/safe-apps-sdk");
-        // If both modules are found, return the custom global variable
-        globalVariable = "global";
-      } catch (e) {
-        // If either module is not found, fallback to globalThis
-        globalVariable = "globalThis";
-      }
-      return globalVariable;
-    })(),
-
+    global: "globalThis",
     "process.env": {},
-  },
-  build: {
-    rollupOptions: {
-      external: ["@safe-globalThis/safe-ethers-adapters","@thirdweb-dev/chains"],
-    },
   },
 });
